8e72236eee9e31e1dbdca14b7021539bba426ea4
Encrypto27 <diegoallies27@gmail.com>
2022-03-08 11:34:22 +0200

    configured auth jwt

--- app/middleware/auth.jwt.js
+++ app/middleware/auth.jwt.js
@@ -1,18 +1,18 @@
-const Product = require('../models/posts')
+const post = require('../models/posts')
  
 getPost =  async (req, res, next) => {
-   let product
+   let post
   try{
-      product = await Product.findById(req.params.id)
-     if(product == null){
-         return res.status(404).json({ message:'This product could not be found' })
+      post = await post.findById(req.params.id)
+     if(post == null){
+         return res.status(404).json({ message:'This post could not be found' })
      } 
   } catch (err) {
       return res.status(500).json({ message: err.message })
   }
 
-  res.product = product
+  res.post = post
   next()
 }
 
-module.exports = getProduct
+module.exports = getPost
