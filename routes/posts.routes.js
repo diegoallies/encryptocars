17958fce38062502d063cb0387b0fecf5f90c065
Encrypto27 <diegoallies27@gmail.com>
2022-03-08 12:14:48 +0200

    added user routes

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -1,23 +1,23 @@
 const express = require('express')
 const router = express.Router()
-const Product = require('../models/product')
+const post = require('../models/post')
 const verifyToken = require('../middleware/auth.jwt')
 
 router.get('/', async (req, res) => {
    try {
-    const products = await Product.find()
-    res.json(products)
+    const posts = await post.find()
+    res.json(posts)
    } catch (err) {
     res.status(500).json({ message: err.message })
    }
 })
 
-router.get('/:id', getProduct , (req, res) => {
-    res.json(res.product)
+router.get('/:id', getpost , (req, res) => {
+    res.json(res.post)
 })
 
 router.post('/',verifyToken, async (req, res) => {
-    const product = new Product({
+    const post = new post({
         title: req.body.title,
         category: req.body.category,
         description: req.body.description,
@@ -26,65 +26,65 @@ router.post('/',verifyToken, async (req, res) => {
        created_by: req.userId
     })
     try{
-        const newProduct = await product.save()
-        res.status(201).json(newProduct)
+        const newpost = await post.save()
+        res.status(201).json(newpost)
     } catch(err){
         res.status(400).json({ message: err.message })
     }
 })
 
-router.patch('/:id',[getProduct,verifyToken], async (req, res) => {
-    if( res.product.created_by != req.userId){
+router.patch('/:id',[getpost,verifyToken], async (req, res) => {
+    if( res.post.created_by != req.userId){
         return res.status(401).send({ message: "Unauthorized!" });
     }
     if(req.body.title !=null){
-        res.product.title =  req.body.title
+        res.post.title =  req.body.title
     }
     if(req.body.category !=null){
-        res.product.category =  req.body.category
+        res.post.category =  req.body.category
     }
     if(req.body.description !=null){
-        res.product.description =  req.body.description
+        res.post.description =  req.body.description
     }
     if(req.body.img !=null){
-        res.product.img =  req.body.img
+        res.post.img =  req.body.img
     }
     if(req.body.price !=null){
-        res.product.price =  req.body.price
+        res.post.price =  req.body.price
     }
     
     try{
-        const updatedProduct = await res.product.save()
-        res.json(updatedProduct)
+        const updatedpost = await res.post.save()
+        res.json(updatedpost)
     } catch (err) {
         res.status(400).json({ message: err.message })
     }
 })
 
-router.delete('/:id',[getProduct,verifyToken], async (req, res) => {
+router.delete('/:id',[getpost,verifyToken], async (req, res) => {
     try{
-        if( res.product.created_by != req.userId){
+        if( res.post.created_by != req.userId){
             return res.status(401).send({ message: "Unauthorized!" });
         }
-        await res.product.remove()
-        res.json({ message:'Deleted Product'})
+        await res.post.remove()
+        res.json({ message:'Deleted post'})
     } catch (err) {
         res.status(500).json({ message: err.message })
     }
 }) 
 
-async function getProduct  (req, res, next){
-    let product
+async function getpost  (req, res, next){
+    let post
    try{
-       product = await Product.findById(req.params.id)
-      if(product == null){
-          return res.status(404).json({ message:'Cannot find product' })
+       post = await post.findById(req.params.id)
+      if(post == null){
+          return res.status(404).json({ message:'Cannot find post' })
       } 
    } catch (err) {
        return res.status(500).json({ message: err.message })
    }
 
-   res.product = product
+   res.post = post
    next()
 }
 
