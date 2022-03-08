c570511e81f73424ba207b3a1dff49a22599c796
Encrypto27 <diegoallies27@gmail.com>
2022-03-08 12:21:42 +0200

    added amd configured server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database Successfully'))
+
+app.get('/',(req,res)=>{
+     res.send({message:"Welcome to Diego and Sive application"})
+ })
+app.use(express.json())
+app.use(cors())
+
+const usersRouter = require('./app/routes/user.routes.js')
+app.use('/users', usersRouter)
+const productsRouter = require('./app/routes/products.routes.js')
+app.use('/products', productsRouter)
+
+app.listen(process.env.PORT || 8999, () => console.log('Server Started'))
