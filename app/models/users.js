863766e99c68c0fb1ba98415d2563767491bfb6f
Encrypto27 <diegoallies27@gmail.com>
2022-03-10 11:56:54 +0200

    fixed login error

--- app/models/users.js
+++ app/models/users.js
@@ -17,6 +17,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    roles:{
+        type: String,
+        required: false
+    },
     join_date:{
         type: String,
         default: Date.now
