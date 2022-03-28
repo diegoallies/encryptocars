186e843c6ad6b639358f71b3a3c2571338413a22
Encrypto27 <diegoallies27@gmail.com>
2022-03-28 15:00:36 +0200

    added more items

--- app/models/posts.js
+++ app/models/posts.js
@@ -1,21 +1,30 @@
 const mongoose = require('mongoose')
 
 const postSchema = new mongoose.Schema({
-    postText:{
+    title:{
         type: String,
         required: true
     },
-
     img:{
         type: String,
         required: true
     },
-
+    category:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
     created_by: {
         type: String,
         required: true
     },
-
     fullname: {
         type: String,
         required: true
